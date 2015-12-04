d0b3b116630206a79d9e98157714938dbbc080b6
Agustin Camino <agustin.camino@gmail.com>
2015-12-03 19:42:09 -0500

    Add Subscriber model

---
+++ MarketingNotifications.Web/Models/Subscriber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketingNotifications.Web.Models
+{
+    public class Subscriber
+    {
+        public int Id { get; set; }
+        public bool Subscribed { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
