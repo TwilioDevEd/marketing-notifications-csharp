372f96442a78b05c9944a9513bf58c7d4a16d93b
Agustin Camino <agustin.camino@gmail.com>
2015-12-04 10:53:04 -0500

    Add subscribers repository

---
+++ MarketingNotifications.Web/Models/Repository/SubscribersRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using MarketingNotifications.Web.Migrations;
+
+namespace MarketingNotifications.Web.Models.Repository
+{
+    public interface ISubscribersRepository
+    {
+        Task<Subscriber> FindByPhoneNumber(string phoneNumber);
+        Task<int> CreateAsync(Subscriber subscriber);
+        Task<int> UpdateAsync(Subscriber subscriber);
+    }
+
+    public class SubscribersRepository : ISubscribersRepository
+    {
+        private readonly MarketingNotificationsContext _context;
+
+        public SubscribersRepository()
+        {
+            _context = new MarketingNotificationsContext();
+        }
+
+        public async Task<Subscriber> FindByPhoneNumber(string phoneNumber)
+        {
+            return await _context.Subscribers.FirstAsync(s => s.PhoneNumber == phoneNumber);
+        }
+
+        public async Task<int> CreateAsync(Subscriber subscriber)
+        {
+            _context.Subscribers.Add(subscriber);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> UpdateAsync(Subscriber subscriber)
+        {
+            _context.Entry(subscriber).State = EntityState.Modified;
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
