7a561e8667df6ece569a19a8dcc98a8df6595290
Agustin Camino <agustin.camino@gmail.com>
2015-12-03 19:45:29 -0500

    Remove unused references

--- MarketingNotifications.Web.Tests/Controllers/HomeControllerTest.cs
+++ MarketingNotifications.Web.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MarketingNotifications.Web;
+﻿using System.Web.Mvc;
 using MarketingNotifications.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MarketingNotifications.Web.Tests.Controllers
 {
