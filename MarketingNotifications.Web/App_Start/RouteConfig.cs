8471c5721291f819ae973d7e53f22b28312bb37b
Agustin Camino <agustin.camino@gmail.com>
2015-12-06 06:19:06 -0500

    Remove home controller
    * Remove related views.
    * Update default route to point to create at notifications controller.

--- MarketingNotifications.Web/App_Start/RouteConfig.cs
+++ MarketingNotifications.Web/App_Start/RouteConfig.cs
@@ -10,9 +10,9 @@ namespace MarketingNotifications.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Notifications", action = "Create", id = UrlParameter.Optional }
             );
         }
     }
