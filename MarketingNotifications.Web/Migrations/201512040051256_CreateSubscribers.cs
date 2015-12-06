2612cff96c97f80f6dad089b85f051357c4c27b1
Agustin Camino <agustin.camino@gmail.com>
2015-12-06 11:25:22 -0500

    Remove unnecessary imports from project

--- MarketingNotifications.Web/Migrations/201512040051256_CreateSubscribers.cs
+++ MarketingNotifications.Web/Migrations/201512040051256_CreateSubscribers.cs
@@ -1,8 +1,7 @@
+using System.Data.Entity.Migrations;
+
 namespace MarketingNotifications.Web.Migrations
 {
-    using System;
-    using System.Data.Entity.Migrations;
-    
     public partial class CreateSubscribers : DbMigration
     {
         public override void Up()
