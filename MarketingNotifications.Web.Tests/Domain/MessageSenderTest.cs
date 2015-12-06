6e49fe74b205a19f56847e2b33f6ab6cc364ba69
Agustin Camino <agustin.camino@gmail.com>
2015-12-05 22:51:29 -0500

    Add message sender

---
+++ MarketingNotifications.Web.Tests/Domain/MessageSenderTest.cs
@@ -0,0 +1,21 @@
+﻿using MarketingNotifications.Web.Domain;
+using Moq;
+using NUnit.Framework;
+using Twilio;
+
+namespace MarketingNotifications.Web.Tests.Domain
+{
+    public class MessageSenderTest
+    {
+        [Test]
+        public void SendMessage()
+        {
+            var mockClient = new Mock<TwilioRestClient>(string.Empty, string.Empty);
+
+            var messageSender = new MessageSender(mockClient.Object);
+            messageSender.Send("555-5555", "message", "message-url");
+
+            mockClient.Verify(c => c.SendMessage(null, "555-5555", "message", "message-url"));
+        }
+    }
+}
---
+++ MarketingNotifications.Web/Domain/MessageSender.cs
@@ -0,0 +1,32 @@
+﻿using MarketingNotifications.Web.Domain.Twilio;
+using Twilio;
+
+namespace MarketingNotifications.Web.Domain
+{
+    public interface IMessageSender
+    {
+        void Send(string to, string body, string imageUrl);
+    }
+
+    public class MessageSender : IMessageSender
+    {
+        private readonly TwilioRestClient _client;
+
+        public MessageSender() :
+            this(new TwilioRestClient(
+                Configuration.Credentials.AccountSID, Configuration.Credentials.AuthToken))
+        {
+        }
+
+        public MessageSender(TwilioRestClient client)
+        {
+            _client = client;
+        }
+
+
+        public void Send(string to, string body, string imageUrl)
+        {
+            _client.SendMessage(Configuration.PhoneNumbers.Twilio, to, body, imageUrl);
+        }
+    }
+}
