5de99320d6af0ff6f4fe590df8ded84a075369a6
Juan Carlos Pazmino <juankpro@outlook.com>
2017-01-06 08:41:32 -0800

    Update Twilio-CSharp to version 5.0.0-rca2

--- MarketingNotifications.Web/Domain/MessageSender.cs
+++ MarketingNotifications.Web/Domain/MessageSender.cs
@@ -1,32 +1,35 @@
 ﻿using MarketingNotifications.Web.Domain.Twilio;
+using System.Collections.Generic;
+using System;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 using Twilio;
 
 namespace MarketingNotifications.Web.Domain
 {
     public interface IMessageSender
     {
-        void Send(string to, string body, params string[] imageUrl);
+        MessageResource Send(string to, string body, List<Uri> mediaUrl);
     }
 
     public class MessageSender : IMessageSender
     {
-        private readonly TwilioRestClient _client;
-
-        public MessageSender() :
-            this(new TwilioRestClient(
-                Configuration.Credentials.AccountSID, Configuration.Credentials.AuthToken))
+        public MessageSender()
         {
+            if (Configuration.Credentials.AccountSID != null && Configuration.Credentials.AuthToken != null)
+            {
+                TwilioClient.Init(Configuration.Credentials.AccountSID, Configuration.Credentials.AuthToken);
+            }
         }
 
-        public MessageSender(TwilioRestClient client)
+        public MessageResource Send(string to, string body, List<Uri> mediaUrl)
         {
-            _client = client;
-        }
-
-
-        public void Send(string to, string body, params string[] imageUrl)
-        {
-            _client.SendMessage(Configuration.PhoneNumbers.Twilio, to, body, imageUrl);
+            return MessageResource.Create(
+                from: new PhoneNumber(Configuration.PhoneNumbers.Twilio),
+                to: new PhoneNumber(to),
+                body: body,
+                mediaUrl: mediaUrl
+                );
         }
     }
 }
