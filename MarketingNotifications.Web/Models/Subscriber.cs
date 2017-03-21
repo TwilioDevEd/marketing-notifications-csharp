85b9efdf71c369d7942e1047567b867f541d0723
Twilio CMS User <pkamp@twilio.com>
2017-03-21 15:36:48 -0700

    CMS update of MarketingNotifications.Web/Models/Subscriber.cs by pkamp@twilio.com

--- MarketingNotifications.Web/Models/Subscriber.cs
+++ MarketingNotifications.Web/Models/Subscriber.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 namespace MarketingNotifications.Web.Models
 {
     public class Subscriber
     {
         public int Id { get; set; }
-        public string PhoneNumber { get; set; }
+        public String PhoneNumber { get; set; }
         public bool Subscribed { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
